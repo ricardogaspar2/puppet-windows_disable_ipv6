d533ad2476975d5f7d3063d84003d33c3cf5099e
martezr <martez.reed@greenreedtech.com>
2014-04-25 13:00:26 -0500

    Comment out reboot message
    The puppet notify message has been commented out until the logic is
    corrected.

--- manifests/init.pp
+++ manifests/init.pp
@@ -15,20 +15,22 @@
 #
 # == Sample Usage:
 #
+# Disable IPv6 and restart the system to apply the change
+#
 #  class {'windows_disable_ipv6':
 #   ipv6_disable => true,
+#   ipv6_reboot  => true,
 #   }
 #
 
 class windows_disable_ipv6 (
-  # ipv6 enabled by default
-  $ipv6_disable = false,
-  $ipv6_reboot  = true,
+  $ipv6_disable = false,  # ipv6 enabled by default
+  $ipv6_reboot  = false,  # do not reboot by default
 ) {
 
 # Validate ipv6 class 
 validate_bool($ipv6_disable)
-validate_bool(ipv6_reboot)
+validate_bool($ipv6_reboot)
 
 # IPv6 logic (IPv6 enabled = 0, IPv6 disabled = 1)
 if $ipv6_disable {
@@ -37,6 +39,7 @@ if $ipv6_disable {
   $ipv6_setting = '0x0'
 }
 
+# Modify the IPv6 registry key
 registry::value { 'ipv6':
   key   => 'HKLM\System\CurrentControlSet\Services\Tcpip6\Parameters',
   value => 'DisabledComponents',
@@ -45,14 +48,16 @@ registry::value { 'ipv6':
 }
 
 if $ipv6_reboot {
+# Monitor for changes to ipv6 registry to determine whether a system restart is required  
 reboot { 'after':
  subscribe => registry::value['ipv6'],
  }
  }
  else{
-notify {'ipv6rebootmessage':
-name    => 'ipv6 reboot message',
-message => 'The ipv6 settings will be applied after the next system restart',
-} 
+## Create puppet log message that they system was not restart during run and requires a restart to apply settings   
+#notify {'ipv6rebootmessage':
+#name    => 'ipv6 reboot message',
+#message => 'The ipv6 settings will be applied after the next system restart',
+#} 
   }
 }
