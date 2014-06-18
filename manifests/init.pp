c48dad4c7e3a75c75d7b156f5f1facc5a6e3cedc
root <martez.reed@greenreedtech.com>
2014-06-17 17:15:29 -0700

    Init.pp style guide compliance

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,8 +1,8 @@
 # Class: windows_disable_ipv6
 #
 #  This class is used to enable/disable IPv6 on Windows platforms
-# 
-# == Parameters: 
+#
+# == Parameters:
 #
 #  $ipv6_disable: Whether IPv6 is enabled. Defaults to false or enabled.
 #  Valid values: true and false
@@ -28,7 +28,7 @@ class windows_disable_ipv6 (
   $ipv6_reboot  = false,  # do not reboot by default
 ) {
 
-# Validate ipv6 class 
+# Validate ipv6 class
 validate_bool($ipv6_disable)
 validate_bool($ipv6_reboot)
 
@@ -48,16 +48,16 @@ registry::value { 'ipv6':
 }
 
 if $ipv6_reboot {
-# Monitor for changes to ipv6 registry to determine whether a system restart is required  
+# Monitor for changes to ipv6 registry to determine whether a system restart is required
 reboot { 'after':
- subscribe => registry::value['ipv6'],
- }
- }
- else{
-## Create puppet log message that they system was not restart during run and requires a restart to apply settings   
+  subscribe => registry::value['ipv6'],
+  }
+  }
+  else{
+## Create puppet log message that they system was not restart during run and requires a restart to apply settings
 #notify {'ipv6rebootmessage':
 #name    => 'ipv6 reboot message',
 #message => 'The ipv6 settings will be applied after the next system restart',
-#} 
+#}
   }
 }
